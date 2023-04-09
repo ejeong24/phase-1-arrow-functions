c9c0ea35de6f2cb76ed2d1d86c16d06a778d29dd
Eugene Tae Joo Jeong <ejeong24@gmail.com>
2023-04-09 19:49:33 -0400

    passed testing

---
+++ index.js
@@ -0,0 +1,5 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide (2000,1000);
+
+const square = (parameter3) => parameter3*parameter3;
+const add = (parameter1, parameter2) => parameter1 + parameter2;
